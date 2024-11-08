385789b973d9712fdf9703b8fe24cf114e6f91f7
Dexaf <federicopisajob@gmail.com>
2024-11-08 11:44:05 +0100

    Added debug utilities with method to draw selfdestructable primitive gameobjects

---
+++ DebugUtilities.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public static class DebugUtilities
+{
+    /// <summary>
+    /// Draw primitive forms like capsule, cubes ecc... for a N time in a location.
+    /// (similar to Debug.DrawRay)
+    /// </summary>
+    public static void DrawPrimitive(Vector3 position, float scale, PrimitiveType primitiveType, Color color, float time = 1f)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(primitiveType);
+        sphere.transform.localScale = new Vector3(scale, scale, scale);
+        sphere.transform.position = position;
+        sphere.GetComponent<Renderer>().material.color = color;
+        sphere.AddComponent<DestroySelf>();
+    }
+}
+
+public class DestroySelf : MonoBehaviour
+{
+    public void Start()
+    {
+        StartCoroutine(SelfDestruct());
+    }
+
+    private IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
+}
